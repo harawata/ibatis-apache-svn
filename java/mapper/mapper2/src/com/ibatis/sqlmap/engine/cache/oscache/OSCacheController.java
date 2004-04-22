85ebb12f7d5d89b4b25a4395885d2c49ce5708e7
cbegin <cbegin@13f79535-47bb-0310-9956-ffa450edef68>
2004-04-22 02:09:56 +0000

    no message
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@152353 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/src/com/ibatis/sqlmap/engine/cache/oscache/OSCacheController.java
+++ java/mapper/mapper2/src/com/ibatis/sqlmap/engine/cache/oscache/OSCacheController.java
@@ -5,64 +5,67 @@
  */
 package com.ibatis.sqlmap.engine.cache.oscache;
 
-import com.ibatis.sqlmap.engine.cache.*;
-import com.opensymphony.oscache.base.*;
-import com.opensymphony.oscache.general.*;
+import com.ibatis.sqlmap.engine.cache.CacheController;
+import com.ibatis.sqlmap.engine.cache.CacheModel;
+import com.opensymphony.oscache.base.NeedsRefreshException;
+import com.opensymphony.oscache.general.GeneralCacheAdministrator;
 
-import java.util.*;
+import java.util.Properties;
 
 public class OSCacheController implements CacheController {
 
-  private static final GeneralCacheAdministrator CACHE = new GeneralCacheAdministrator();
+    private static final GeneralCacheAdministrator CACHE = new GeneralCacheAdministrator();
 
-  public void flush(CacheModel cacheModel) {
-    synchronized (cacheModel) {
-      CACHE.flushGroup(cacheModel.getId());
-    }
-  }
-
-  public Object getObject(CacheModel cacheModel, Object key) {
-    String keyString = key.toString();
-    try {
-      synchronized (cacheModel) {
-        int refreshPeriod = (int) (cacheModel.getFlushIntervalSeconds());
-        return CACHE.getFromCache(keyString, refreshPeriod);
-      }
-    } catch (NeedsRefreshException e) {
-      CACHE.cancelUpdate(keyString);
-      return null;
-    }
-  }
-
-  public synchronized Object removeObject(CacheModel cacheModel, Object key) {
-    String keyString = key.toString();
-    try {
-      synchronized (cacheModel) {
-        int refreshPeriod = (int) (cacheModel.getFlushIntervalSeconds());
-        Object value = CACHE.getFromCache(keyString, refreshPeriod);
-        if (value != null) {
-          CACHE.flushEntry(keyString);
+    public void flush(CacheModel cacheModel) {
+        synchronized (cacheModel) {
+            CACHE.flushGroup(cacheModel.getId());
         }
-        return value;
-      }
-    } catch (NeedsRefreshException e) {
-      try {
-        CACHE.flushEntry(keyString);
-      } finally {
-        CACHE.cancelUpdate(keyString);
-        return null;
-      }
     }
-  }
 
-  public void putObject(CacheModel cacheModel, Object key, Object object) {
-    String keyString = key.toString();
-    synchronized (cacheModel) {
-      CACHE.putInCache(keyString, object, new String[]{cacheModel.getId()});
+    public Object getObject(CacheModel cacheModel, Object key) {
+        String keyString = key.toString();
+        try {
+            synchronized (cacheModel) {
+                int refreshPeriod = (int) (cacheModel.getFlushIntervalSeconds());
+                return CACHE.getFromCache(keyString, refreshPeriod);
+            }
+        } catch (NeedsRefreshException e) {
+            CACHE.cancelUpdate(keyString);
+            return null;
+        }
     }
-  }
 
-  public void configure(Properties props) {
-  }
+    public synchronized Object removeObject(CacheModel cacheModel, Object key) {
+        Object result;
+        String keyString = key.toString();
+        try {
+            synchronized (cacheModel) {
+                int refreshPeriod = (int) (cacheModel.getFlushIntervalSeconds());
+                Object value = CACHE.getFromCache(keyString, refreshPeriod);
+                if (value != null) {
+                    CACHE.flushEntry(keyString);
+                }
+                result = value;
+            }
+        } catch (NeedsRefreshException e) {
+            try {
+                CACHE.flushEntry(keyString);
+            } finally {
+                CACHE.cancelUpdate(keyString);
+                result = null;
+            }
+        }
+        return result;
+    }
+
+    public void putObject(CacheModel cacheModel, Object key, Object object) {
+        String keyString = key.toString();
+        synchronized (cacheModel) {
+            CACHE.putInCache(keyString, object, new String[]{cacheModel.getId()});
+        }
+    }
+
+    public void configure(Properties props) {
+    }
 
 }
