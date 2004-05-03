a6f4e1dc5c7ede87fd73d4f1891dd36aaec85c3d
cbegin <cbegin@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-03 01:01:31 +0000

    bug fix (better error message for unsupported types)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@152383 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/src/com/ibatis/sqlmap/engine/mapping/result/BasicResultMap.java
+++ java/mapper/mapper2/src/com/ibatis/sqlmap/engine/mapping/result/BasicResultMap.java
@@ -270,18 +270,22 @@ public class BasicResultMap implements ResultMap {
   }
 
   protected Object getPrimitiveResultMappingValue(ResultSet rs, BasicResultMapping mapping) throws SQLException {
-    TypeHandler typeHandler = mapping.getTypeHandler();
-    String columnName = mapping.getColumnName();
-    int columnIndex = mapping.getColumnIndex();
-    String nullValue = mapping.getNullValue();
     Object value = null;
-    if (columnName == null) {
-      value = typeHandler.getResult(rs, columnIndex);
+    TypeHandler typeHandler = mapping.getTypeHandler();
+    if (typeHandler != null) {
+      String columnName = mapping.getColumnName();
+      int columnIndex = mapping.getColumnIndex();
+      String nullValue = mapping.getNullValue();
+      if (columnName == null) {
+        value = typeHandler.getResult(rs, columnIndex);
+      } else {
+        value = typeHandler.getResult(rs, columnName);
+      }
+      if (value == null && nullValue != null) {
+        value = typeHandler.valueOf(nullValue);
+      }
     } else {
-      value = typeHandler.getResult(rs, columnName);
-    }
-    if (value == null && nullValue != null) {
-      value = typeHandler.valueOf(nullValue);
+      throw new SqlMapException("No type handler could be found to map the property '" + mapping.getPropertyName() + "' to the column '" + mapping.getColumnName() + "'.  One or both of the types, or the combination of types is not supported.");
     }
     return value;
   }
