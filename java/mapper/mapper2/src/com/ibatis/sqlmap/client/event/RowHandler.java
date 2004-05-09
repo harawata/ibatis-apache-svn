2c9606e6f338198b34ac1147177ae8021765785c
cbegin <cbegin@13f79535-47bb-0310-9956-ffa450edef68>
2004-05-09 08:53:11 +0000

    doc update
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@152392 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/src/com/ibatis/sqlmap/client/event/RowHandler.java
+++ java/mapper/mapper2/src/com/ibatis/sqlmap/client/event/RowHandler.java
@@ -3,14 +3,13 @@ package com.ibatis.sqlmap.client.event;
 /**
  * Event handler for row by row processing.
  * <p/>
- * The RowHandler interface is used by the SqlMapSession.queryForList() method.
+ * The RowHandler interface is used by the SqlMapSession.queryWithRowHandler() method.
  * Generally a RowHandler implementation will perform some row-by-row processing logic
- * in cases where there are too many rows to efficiently load into memory.  Alternatively,
- * the RowHandler can also be used to build lists using custom logic.
+ * in cases where there are too many rows to efficiently load into memory.
  * <p/>
  * Example:
  * <pre>
- * List list = sqlMap.queryForList ("findAllEmployees", null, new MyRowHandler()));
+ * sqlMap.queryWithRowHandler ("findAllEmployees", null, new MyRowHandler()));
  * </pre>
  * Date: Sep 5, 2003 4:46:44 PM
  *
@@ -23,9 +22,7 @@ public interface RowHandler {
    * <p/>
    * This method will be called for each row in a result set.  For each row the result map
    * will be applied to build the value object, which is then passed in as the valueObject
-   * parameter.  An instantiated list is provided as the second parameter that you are free
-   * to add none, some or all of the value objects to.  The list will be returned by
-   * SqlMapSession.queryForList() after all rows have been processed.
+   * parameter.
    *
    * @param valueObject The object representing a single row from the query.
    * @see com.ibatis.sqlmap.client.SqlMapSession
