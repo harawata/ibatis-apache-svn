90cd2eedf73fdf3dbfddc0e5f46e4bd8ec51db4d
cbegin <cbegin@13f79535-47bb-0310-9956-ffa450edef68>
2004-11-27 08:03:39 +0000

    removed template comments
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@152569 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/test/com/ibatis/sqlmap/engine/cache/memory/MemoryCacheControllerTest.java
+++ java/mapper/mapper2/test/com/ibatis/sqlmap/engine/cache/memory/MemoryCacheControllerTest.java
@@ -1,29 +1,19 @@
-/*
- * Created on Aug 31, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package com.ibatis.sqlmap.engine.cache.memory;
 
 import com.ibatis.sqlmap.engine.cache.CacheController;
 import com.ibatis.sqlmap.engine.cache.lru.LruCacheControllerTest;
 
-/**
- * @author lmeadors
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 public class MemoryCacheControllerTest extends LruCacheControllerTest {
 
   public static void main(String[] args) {
     junit.textui.TestRunner.run(MemoryCacheControllerTest.class);
   }
-  protected CacheController getController(){
+
+  protected CacheController getController() {
     return new MemoryCacheController();
   }
+
   public void testSizeOne() {
-      // This is not relevant for this model
+    // This is not relevant for this model
   }
 }
