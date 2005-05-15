70584cd70e4e34ead52eb1458c501ef1a48caf36
roberto <roberto@13f79535-47bb-0310-9956-ffa450edef68>
2005-05-15 20:20:24 +0000

    +Adding C# DataMapper NUnit PostgreSQL scripts (first cut!)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@170260 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cs/mapper/IBatisNet.DataMapper.Test/Scripts/PostgreSQL/category-init.sql
@@ -0,0 +1,23 @@
+\connect "IBatisNet" "IBatisNet";
+
+DROP SEQUENCE "S_Categories";
+
+CREATE SEQUENCE "S_Categories"
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 20;
+ALTER TABLE "S_Categories" OWNER TO "IBatisNet";
+
+DROP TABLE "Categories";
+
+CREATE TABLE "Categories"
+(
+  "Category_Id" int4 NOT NULL,
+  "Category_Name" varchar(32),
+  "Category_Guid" varchar(36),
+  CONSTRAINT "PK_Categories" PRIMARY KEY ("Category_Id")
+) 
+WITHOUT OIDS;
+ALTER TABLE "Categories" OWNER TO "IBatisNet";
---
+++ cs/mapper/IBatisNet.DataMapper.Test/Scripts/PostgreSQL/user-init.sql
@@ -0,0 +1,15 @@
+\connect "NHibernate" "NHibernate";
+
+DROP TABLE "Users";
+
+CREATE TABLE "Users"
+(
+  "LogonId" varchar(20) NOT NULL DEFAULT '0'::character varying,
+  "Name" varchar(40),
+  "Password" varchar(20),
+  "EmailAddress" varchar(40),
+  "LastLogon" date,
+  CONSTRAINT "PK_Users" PRIMARY KEY ("LogonId")
+) 
+WITHOUT OIDS;
+ALTER TABLE "Users" OWNER TO "NHibernate";
