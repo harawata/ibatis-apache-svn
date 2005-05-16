965dd715910625dc1e72de5ab78da627c70eb0ac
roberto <roberto@13f79535-47bb-0310-9956-ffa450edef68>
2005-05-16 05:25:36 +0000

    ~Updated C# DataMapper NUnit more-account-records.sql scripts and other cleaning
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ibatis/trunk@170310 13f79535-47bb-0310-9956-ffa450edef68

--- cs/mapper/IBatisNet.DataMapper.Test/Scripts/MSSQL/more-account-records.sql
+++ cs/mapper/IBatisNet.DataMapper.Test/Scripts/MSSQL/more-account-records.sql
@@ -3,9 +3,9 @@
 
 -- Creating Test Data
 
-INSERT INTO [dbo].[Accounts] VALUES(6,'Jane', 'Calamity', 'Jane.Calamity@somewhere.com');
-INSERT INTO [dbo].[Accounts] VALUES(7,'Lucky', 'Luke', 'Lucky.Luke@somewhere.com');
-INSERT INTO [dbo].[Accounts] VALUES(8,'Ming', 'Li Foo', null);
-INSERT INTO [dbo].[Accounts] VALUES(9,'O''Hara', 'Steve', 'Jack.OHara@somewhere.com');
-INSERT INTO [dbo].[Accounts] VALUES(10,'Robert', 'O''Timmins', null);
+INSERT INTO [dbo].[Accounts] VALUES(6,'Jane', 'Calamity', 'Jane.Calamity@somewhere.com', 'Oui', 200);
+INSERT INTO [dbo].[Accounts] VALUES(7,'Lucky', 'Luke', 'Lucky.Luke@somewhere.com', 'Oui', 200);
+INSERT INTO [dbo].[Accounts] VALUES(8,'Ming', 'Li Foo', null, 'Non', 100);
+INSERT INTO [dbo].[Accounts] VALUES(9,'O''Hara', 'Steve', 'Jack.OHara@somewhere.com', 'Oui', 200);
+INSERT INTO [dbo].[Accounts] VALUES(10,'Robert', 'O''Timmins', null, 'Non', 100);
 
