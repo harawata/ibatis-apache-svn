5f33b64be5ccdc390ff6b5e198c130006883720b
gbayon <gbayon@13f79535-47bb-0310-9956-ffa450edef68>
2005-10-14 19:19:03 +0000

    - Improved test performance
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@321188 13f79535-47bb-0310-9956-ffa450edef68

--- cs/mapper/IBatisNet.DataAccess.Test/NUnit/DaoTests/SqlMapDaoTest.cs
+++ cs/mapper/IBatisNet.DataAccess.Test/NUnit/DaoTests/SqlMapDaoTest.cs
@@ -12,17 +12,23 @@ namespace IBatisNet.DataAccess.Test.NUnit.DaoTests
 		/// <summary>
 		/// Initialisation
 		/// </summary>
-		[SetUp] 
-		public void SetUp() 
+		[TestFixtureSetUp] 
+		public void FixtureSetUp() 
 		{
 			DomDaoManagerBuilder builder = new DomDaoManagerBuilder();
 			builder.Configure( "dao"+ "_" + ConfigurationSettings.AppSettings["database"] + "_"
 				+ ConfigurationSettings.AppSettings["providerType"] + ".config" );
-			daoManager = DaoManager.GetInstance("SqlMapDao");
-			
-			InitScript( daoManager.LocalDataSource, ScriptDirectory + "account-init.sql" );
+			daoManager = DaoManager.GetInstance("SqlMapDao");		
 		}
 
+		/// <summary>
+		/// Initialisation
+		/// </summary>
+		[SetUp] 
+		public void SetUp() 
+		{			
+			InitScript( daoManager.LocalDataSource, ScriptDirectory + "account-init.sql" );
+		}
 
 	}
 }
