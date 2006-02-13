07cb28d36f3f2a2c332d5ba745b421d1a5988378
sboden <sboden@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-13 18:08:19 +0000

    Fix for IBATIS-250: SELECT statement returns unexpected result when 'groupBy' and 'nullValue' are specified in resultMaps.
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@377443 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/test/com/ibatis/sqlmap/RepeatingGroupMappingTest.java
+++ java/mapper/mapper2/test/com/ibatis/sqlmap/RepeatingGroupMappingTest.java
@@ -34,6 +34,30 @@ public class RepeatingGroupMappingTest extends BaseSqlMapTest {
 
     Product product = (Product)cat.getProductList().get(0);
     assertEquals (2, product.getItemList().size());
+  }
 
+  /**
+   * This is a test case for iBATIS JIRA-250 "SELECT statement
+   * returns unexpected result when 'groupBy' and 'nullValue'
+   * are specified in resultMaps."
+   *
+   * The problem was that when a child object in a resultmap only
+   * contained null values it would still be created when one of the
+   * properties of the child object contained a nullValue attribute
+   * in the ResultMap. The nullValue would be applied before checking
+   * whether all properties of the child were 'null', so at least 1
+   * property would always be non-null (the reason for the
+   * nullValue attribute).
+   *
+   * The fix is to first check whether all properties of the child are
+   * 'null', and if the child object contains at least 1 non-null property
+   * to then only create the child object and apply the nullValue attribute.
+   *
+   * @throws Exception none should be thrown.
+   */
+  public void testGroupByJIRA250() throws Exception {
+    List list = sqlMap.queryForList("getAllProductCategoriesJIRA250",null);
+    Category cat = (Category)list.get(0);
+    assertEquals (0, cat.getProductList().size());
   }
 }
