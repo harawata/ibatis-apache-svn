17c32ae8506abad49b1805c826cbdb8110041741
jgbutler <jgbutler@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-16 17:19:19 +0000

    Abator Build Improvements
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@378306 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/tools/abator/core/build/setenv.bat
+++ java/mapper/mapper2/tools/abator/core/build/setenv.bat
@@ -1,6 +1,5 @@
 set BUILD_CP=%CLASSPATH%
 set BUILD_CP=%BUILD_CP%;%JAVA_HOME%\lib\tools.jar;
 set BUILD_CP=%BUILD_CP%..\devlib\ant.jar;
-set BUILD_CP=%BUILD_CP%..\devlib\ant-launcher.jar;
 set BUILD_CP=%BUILD_CP%..\devlib\ant-nodeps.jar;
 echo %BUILD_CP%
