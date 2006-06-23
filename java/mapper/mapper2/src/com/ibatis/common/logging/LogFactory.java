ff087729e5c9839d21d21d0523f6fb189001a801
jgbutler <jgbutler@13f79535-47bb-0310-9956-ffa450edef68>
2006-06-23 16:10:48 +0000

    Changes for IBATIS-305 - add some methods to select specific logging implementations
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@416755 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/src/com/ibatis/common/logging/LogFactory.java
+++ java/mapper/mapper2/src/com/ibatis/common/logging/LogFactory.java
@@ -32,4 +32,40 @@ public class LogFactory {
     }
   }
 
+  /**
+   * This method will switch the logging implementation to Log4J if
+   * Log4J is available on the classpath.  This is useful in situations
+   * where you want to use Log4J to log iBATIS activity but
+   * commons logging is on the classpath.  Note that this method is only
+   * effective for log classes obtained after calling this method.  If you
+   * intend to use this method you should call it before calling any other
+   * iBATIS method.
+   *
+   */
+  public static synchronized void selectLog4JLogging() {
+    try {
+      Class.forName("org.apache.log4j.Logger");
+      Class implClass = Class.forName("com.ibatis.common.logging.log4j.Log4jImpl");
+      logConstructor = implClass.getConstructor(new Class[]{Class.class});
+    } catch (Throwable t) {
+    }
+  }
+  
+  /**
+   * This method will switch the logging implementation to Java native logging if
+   * you are running in JRE 1.4 or above.  This is useful in situations
+   * where you want to use Java native logging to log iBATIS activity but
+   * commons logging or Log4J is on the classpath.  Note that this method is only
+   * effective for log classes obtained after calling this method.  If you
+   * intend to use this method you should call it before calling any other
+   * iBATIS method.
+   */
+  public static synchronized void selectJavaLogging() {
+    try {
+      Class.forName("java.util.logging.Logger");
+      Class implClass = Class.forName("com.ibatis.common.logging.jdk14.Jdk14LoggingImpl");
+      logConstructor = implClass.getConstructor(new Class[]{Class.class});
+    } catch (Throwable t) {
+    }
+  }
 }
