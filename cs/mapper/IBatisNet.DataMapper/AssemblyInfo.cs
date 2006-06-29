98b2cda8d4802c44c950c5003426021274273940
gbayon <gbayon@13f79535-47bb-0310-9956-ffa450edef68>
2006-06-29 21:16:40 +0000

    - Updated assemblyinfo.cs for .NET 2.0 and next release
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@418139 13f79535-47bb-0310-9956-ffa450edef68

--- cs/mapper/IBatisNet.DataMapper/AssemblyInfo.cs
+++ cs/mapper/IBatisNet.DataMapper/AssemblyInfo.cs
@@ -1,4 +1,29 @@
+using System;
 using System.Reflection;
+
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+#if dotnet2
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyCompany("http://ibatis.apache.org/")]
+[assembly: AssemblyProduct("iBATIS.NET")]
+[assembly: AssemblyCopyright("Copyright 2006,2005 The Apache Software Foundation")]
+[assembly: AssemblyTrademark("Licensed under the Apache License, Version 2.0")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+#else
+[assembly: AssemblyConfiguration("net-2.0.win32; Release")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("..\\..\\..\\AssemblyKey.snk")]
+#endif
+
+#endif
 
 //
 // General Information about an assembly is controlled through the following 
