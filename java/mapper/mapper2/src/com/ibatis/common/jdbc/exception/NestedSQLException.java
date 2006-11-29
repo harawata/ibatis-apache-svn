5d0d49187f0fec6f1c1b6cfdbee41f1094bdf4c0
jgbutler <jgbutler@13f79535-47bb-0310-9956-ffa450edef68>
2006-11-29 21:24:33 +0000

    Changes for IBATIS-338: NestedSqlException stack overflow
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@480716 13f79535-47bb-0310-9956-ffa450edef68

--- java/mapper/mapper2/src/com/ibatis/common/jdbc/exception/NestedSQLException.java
+++ java/mapper/mapper2/src/com/ibatis/common/jdbc/exception/NestedSQLException.java
@@ -22,10 +22,6 @@ import java.sql.SQLException;
  */
 public class NestedSQLException extends SQLException {
 
-  private static final String CAUSED_BY = "\nCaused by: ";
-
-  private Throwable cause = null;
-
   /**
    * Constructor from java.sql.SQLException
    * @see java.sql.SQLException
@@ -63,7 +59,7 @@ public class NestedSQLException extends SQLException {
    */
   public NestedSQLException(String msg, Throwable cause) {
     super(msg);
-    this.cause = cause;
+    initCause(cause);
   }
 
   /**
@@ -75,7 +71,7 @@ public class NestedSQLException extends SQLException {
    */
   public NestedSQLException(String reason, String SQLState, Throwable cause) {
     super(reason, SQLState);
-    this.cause = cause;
+    initCause(cause);
   }
 
   /**
@@ -87,7 +83,7 @@ public class NestedSQLException extends SQLException {
    */
   public NestedSQLException(String reason, String SQLState, int vendorCode, Throwable cause) {
     super(reason, SQLState, vendorCode);
-    this.cause = cause;
+    initCause(cause);
   }
 
   /**
@@ -96,47 +92,6 @@ public class NestedSQLException extends SQLException {
    */
   public NestedSQLException(Throwable cause) {
     super();
-    this.cause = cause;
+    initCause(cause);
   }
-
-  /**
-   * Gets the causing exception, if any.
-   */
-  public Throwable getCause() {
-    return cause;
-  }
-
-  public String toString() {
-    if (cause == null) {
-      return super.toString();
-    } else {
-      return super.toString() + CAUSED_BY + cause.toString();
-    }
-  }
-
-  public void printStackTrace() {
-    super.printStackTrace();
-    if (cause != null) {
-      System.err.println(CAUSED_BY);
-      cause.printStackTrace();
-    }
-  }
-
-  public void printStackTrace(java.io.PrintStream ps) {
-    super.printStackTrace(ps);
-    if (cause != null) {
-      ps.println(CAUSED_BY);
-      cause.printStackTrace(ps);
-    }
-  }
-
-  public void printStackTrace(java.io.PrintWriter pw) {
-    super.printStackTrace(pw);
-    if (cause != null) {
-      pw.println(CAUSED_BY);
-      cause.printStackTrace(pw);
-    }
-  }
-
-
 }
