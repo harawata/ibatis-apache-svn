48d2a61ea26a5c62ea7bf2a482f520011528161a
gbayon <gbayon@13f79535-47bb-0310-9956-ffa450edef68>
2007-02-01 20:41:36 +0000

    - Updated site for release
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@502349 13f79535-47bb-0310-9956-ffa450edef68

---
+++ site/template/ShowHide.js
@@ -0,0 +1,11 @@
+function ShowHide(id) {
+	var elm = document.getElementById(id);
+	if (elm.style.display == "none")
+	{
+		elm.style.display = "block";
+	}
+	else
+	{
+		elm.style.display = "none";
+	}
+}	
