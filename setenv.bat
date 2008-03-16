23ea9e597e0e89c942dc5bf0bca4360e22ae809b
gbayon <gbayon@13f79535-47bb-0310-9956-ffa450edef68>
2008-03-16 08:41:06 +0000

    cleaning
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk/site@637563 13f79535-47bb-0310-9956-ffa450edef68

--- setenv.bat
+++ setenv.bat
@@ -1,7 +1,7 @@
-
+set CLASSPATH= G:\iBATIS\trunk\site\lib
 set BUILD_CP=%CLASSPATH%
-set BUILD_CP=%BUILD_CP%;%JAVA_HOME%\lib\tools.jar;
+set BUILD_CP=%BUILD_CP%;E:\Java\jdk1.5.0_07\lib\tools.jar;
 
-set BUILD_CP=%BUILD_CP%;lib\ant-1.5.1.jar
+set BUILD_CP=%BUILD_CP%;G:\iBATIS\trunk\site\lib\ant-1.5.1.jar
 
 echo %BUILD_CP%
