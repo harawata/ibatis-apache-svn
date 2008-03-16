3868ed76e3b5020436edef659286710463812f8a
gbayon <gbayon@13f79535-47bb-0310-9956-ffa450edef68>
2008-03-16 08:53:40 +0000

    revert incorrect change
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@637569 13f79535-47bb-0310-9956-ffa450edef68

--- site/ant.bat
+++ site/ant.bat
@@ -1,4 +1,4 @@
 
 call setenv.bat
 
-"E:\Program Files\Java\jdk1.5.0_07\bin\java" -classpath %BUILD_CP% org.apache.tools.ant.Main -buildfile build.xml %1
+%JAVA_HOME%\bin\java -classpath %BUILD_CP% org.apache.tools.ant.Main -buildfile build.xml %1
