8d2e1576b812b52daeeb4fa8e5a0f453823e112d
cbegin <cbegin@13f79535-47bb-0310-9956-ffa450edef68>
2009-06-16 04:33:30 +0000

    added new dynamic sql test
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@785075 13f79535-47bb-0310-9956-ffa450edef68

---
+++ java/ibatis-3/ibatis-3-core/src/test/java/org/apache/ibatis/submitted/dynsql/CreateDB.sql
@@ -0,0 +1,12 @@
+
+create table names (
+  id int,
+  description varchar(20)
+);
+
+insert into names (id, description) values(1, 'Fred');
+insert into names (id, description) values(2, 'Wilma');
+insert into names (id, description) values(3, 'Pebbles');
+insert into names (id, description) values(4, 'Barney');
+insert into names (id, description) values(5, 'Betty');
+insert into names (id, description) values(6, 'Bamm Bamm');
