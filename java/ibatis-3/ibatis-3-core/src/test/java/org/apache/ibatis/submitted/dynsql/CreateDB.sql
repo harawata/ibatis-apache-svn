cb06dea961921600479115c04ff12c132db0b864
jgbutler <jgbutler@13f79535-47bb-0310-9956-ffa450edef68>
2009-06-23 16:35:17 +0000

    Fix SQL error on a submitted test
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@787748 13f79535-47bb-0310-9956-ffa450edef68

--- java/ibatis-3/ibatis-3-core/src/test/java/org/apache/ibatis/submitted/dynsql/CreateDB.sql
+++ java/ibatis-3/ibatis-3-core/src/test/java/org/apache/ibatis/submitted/dynsql/CreateDB.sql
@@ -1,12 +1,13 @@
+create schema ibtest authorization dba;
 
-create table names (
+create table ibtest.names (
   id int,
   description varchar(20)
 );
 
-insert into names (id, description) values(1, 'Fred');
-insert into names (id, description) values(2, 'Wilma');
-insert into names (id, description) values(3, 'Pebbles');
-insert into names (id, description) values(4, 'Barney');
-insert into names (id, description) values(5, 'Betty');
-insert into names (id, description) values(6, 'Bamm Bamm');
+insert into ibtest.names (id, description) values(1, 'Fred');
+insert into ibtest.names (id, description) values(2, 'Wilma');
+insert into ibtest.names (id, description) values(3, 'Pebbles');
+insert into ibtest.names (id, description) values(4, 'Barney');
+insert into ibtest.names (id, description) values(5, 'Betty');
+insert into ibtest.names (id, description) values(6, 'Bamm Bamm');
