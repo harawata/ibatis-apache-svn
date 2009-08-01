44c8ed8a0bb626ee53ace63f7bd6147df4c5551b
jgbutler <jgbutler@13f79535-47bb-0310-9956-ffa450edef68>
2009-08-01 18:17:14 +0000

    Fix bug in the set tag
    git-svn-id: https://svn.apache.org/repos/asf/ibatis/trunk@799904 13f79535-47bb-0310-9956-ffa450edef68

--- java/ibatis-3/ibatis-3-core/src/main/java/org/apache/ibatis/builder/xml/dynamic/SetSqlNode.java
+++ java/ibatis-3/ibatis-3-core/src/main/java/org/apache/ibatis/builder/xml/dynamic/SetSqlNode.java
@@ -3,7 +3,7 @@ package org.apache.ibatis.builder.xml.dynamic;
 public class SetSqlNode extends TrimSqlNode {
 
   public SetSqlNode(SqlNode contents) {
-    super(contents, "SET", null, null, ",");
+    super(contents, "SET", ",", null, null);
   }
 
 }
